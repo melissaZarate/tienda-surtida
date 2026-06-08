79684b2a14c9097ea446bdec4198e5a1c4e3fd07
melissaZarate <meli.za1009@gmail.com>
2026-06-07 22:44:27 -0400

    creacion inicial del proyecto

---
+++ tienda-surtida/src/test/java/com/tiendasurtida/TiendaSurtidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendasurtida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaSurtidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
