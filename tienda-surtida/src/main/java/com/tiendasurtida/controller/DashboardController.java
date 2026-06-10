d63059b8e2498ccb5cdf5ef8cd2804cb9e5f1b08
melissaZarate <meli.za1009@gmail.com>
2026-06-10 02:57:32 -0400

    completado plantilla adminlte con Thymeleaf

--- tienda-surtida/src/main/java/com/tiendasurtida/controller/DashboardController.java
+++ tienda-surtida/src/main/java/com/tiendasurtida/controller/DashboardController.java
@@ -1,4 +1,15 @@
 package com.tiendasurtida.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
 public class DashboardController {
+
+    @GetMapping("/")
+    public String inicio(Model model) {
+        model.addAttribute("contenido","dashboard/index :: contenido")
+        return "layout/base";
+    }
 }
